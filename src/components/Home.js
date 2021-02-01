7d5552e845107b38be5c108bb3db1aa91992e850
Mark Ghaida <markghaida@gmail.com>
2021-01-31 22:21:10 -0500

    commit tree 6ea13d28a531af95b74e7361fc56a4e7ed7d2241

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,9 +3,7 @@ import React from "react";
 function Home(props) {
   return (
     <div id="home">
-      <h1 style={{ color: props.color }}>
-        {props.username} is a Web Developer from {props.city}
-      </h1>
+      <h1 style={{ color: props.color }}> { props.name } is a Web Developer from { props.city } </h1>
     </div>
   );
 }
