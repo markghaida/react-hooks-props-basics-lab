83207b78bb4c0f0be627c19beecc38edb0c1f99d
Mark Ghaida <markghaida@gmail.com>
2021-01-31 22:21:26 -0500

    done

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,18 @@
 import React from "react";
+import Links from "./Links";
 
-function About() {
+function About(props) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      { props.bio && props.bio.length > 1 ? <p>{props.bio}</p> :null}
+      {/* i dont understand the purpose of :null on ln 7 */}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      {/* add your <Links /> component here  */}
+
+      {/* why do you have to again put in the attributes github and linkedin?
+      what is the purpose since you already have it in the Links.js */}
+      <Links github={props.links.github} linkedin={props.links.linkedin}/>
     </div>
   );
 }
